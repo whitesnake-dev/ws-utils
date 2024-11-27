8d03e1bb0052dd0aa60f715a328c0998837d3569
segabelka <Segabelka@yandex.ru>
2024-11-27 12:44:46 +0300

    add some custom hooks for react packages

---
+++ packages/react/use-callback-ref/index.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+
+function useCallbackRef<T extends (...args: any[]) => any>(
+	callback: T | undefined
+): T {
+	const callbackRef = React.useRef(callback);
+
+	React.useEffect(() => {
+		callbackRef.current = callback;
+	});
+
+	return React.useMemo(
+		() => ((...args) => callbackRef.current?.(...args)) as T,
+		[]
+	);
+}
+
+export { useCallbackRef }
